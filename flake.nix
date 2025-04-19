13f0aa60eeee495d746c1abbcce52c52a3e0372a
Lukas Wurzinger <lukas@wrz.one>
2025-04-19 12:43:12 +0200

    update

--- flake.nix
+++ flake.nix
@@ -14,14 +14,15 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
 
-      flake = {
-        lib = nixpkgs.lib.extend (import ./lib.nix);
-
-        nixosConfigurations = self.lib.genNixosConfigurations {inherit inputs;};
-      };
-
-      perSystem = {pkgs, ...}: {
-        packages.default = pkgs.callPackage ./package.nix {};
+      perSystem = {lib, pkgs, ...}: {
+        packages = let
+          packages = lib.packagesFromDirectoryRecursive {
+            inherit (pkgs) callPackage;
+            directory = ./packages;
+          };
+        in packages // {
+          default = packages.helix;
+        };
       };
     };
 }
